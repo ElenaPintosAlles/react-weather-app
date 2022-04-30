b65b49333722cc6a31327ddf48829e6acea7cb2d
ElenaPintosAlles <92037978+ElenaPintosAlles@users.noreply.github.com>
2022-04-30 22:35:48 +0200

    added several day forecast data

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={coedeMapping[props.code]}
       color="#1e1e1e"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
